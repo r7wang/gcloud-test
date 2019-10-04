0cc0cc935a3dc2f9048fb1a6ffc7705383f4e156
Ray Wang <ray.wang@kik.com>
2019-10-03 23:46:26 -0400

    Add simple read row and simple query test cases.

---
+++ spanner/workflow/const.go
@@ -0,0 +1,7 @@
+package workflow
+
+const TransactionTableName = "Transactions"
+
+// NumSamples is the number of times we want to run any given experiment so we can get a good
+// distribution of results.
+const NumSamples = 1000
--- spanner/workflow/oltp.go
+++ spanner/workflow/oltp.go
@@ -7,6 +7,7 @@ import (
 
 	"cloud.google.com/go/spanner"
 	"github.com/r7wang/gcloud-test/spanner/timer"
+	"google.golang.org/api/iterator"
 )
 
 // OLTP defines operations to exercise common types of transactional workflows that define certain
@@ -25,6 +26,18 @@ func NewOLTP(ctx context.Context, client *spanner.Client) *OLTP {
 func (wf *OLTP) Run() error {
 	r := rand.New(rand.NewSource(rand.Int63()))
 
+	if err := wf.simpleReadRow(r); err != nil {
+		return err
+	}
+
+	if err := wf.simpleQuery(r); err != nil {
+		return err
+	}
+
+	if err := wf.multiRead(r); err != nil {
+		return err
+	}
+
 	if err := wf.blindWrite(r); err != nil {
 		return err
 	}
@@ -36,14 +49,69 @@ func (wf *OLTP) Run() error {
 	return nil
 }
 
-func (wf *OLTP) blindWrite(r *rand.Rand) error {
-	const tableName = "Transactions"
-	const numSamples = 1000
+// Read a single row using ReadRow.
+func (wf *OLTP) simpleReadRow(r *rand.Rand) error {
+	const numTransactions = 20000000
+	const baseTransactionID int64 = 1000000000000000000
 
-	for i := 0; i < numSamples; i++ {
+	for i := 0; i < NumSamples; i++ {
+		updateID := baseTransactionID + (r.Int63() % numTransactions)
+		start := time.Now()
+		_, err := wf.client.Single().ReadRow(wf.ctx, TransactionTableName, spanner.Key{updateID}, []string{"fromUserId", "toUserId"})
+		timer.Track(start, "OLTP.simpleReadRow.SQL")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Read a single row using the Query and DML.
+func (wf *OLTP) simpleQuery(r *rand.Rand) error {
+	const numTransactions = 20000000
+	const baseTransactionID int64 = 1000000000000000000
+
+	for i := 0; i < NumSamples; i++ {
+		updateID := baseTransactionID + (r.Int63() % numTransactions)
+		stmt := spanner.Statement{
+			SQL: `SELECT t.FromUserId, t.ToUserId
+					FROM Transactions t
+					WHERE t.Id = @id`,
+			Params: map[string]interface{}{
+				"id": updateID,
+			},
+		}
+		start := time.Now()
+		iter := wf.client.Single().Query(wf.ctx, stmt)
+		for {
+			row, err := iter.Next()
+			if err != nil {
+				if err == iterator.Done {
+					break
+				}
+				return err
+			}
+			var fromUserID, toUserID int64
+			if err := row.Columns(&fromUserID, &toUserID); err != nil {
+				return err
+			}
+		}
+		timer.Track(start, "OLTP.simpleQuery.SQL")
+	}
+	return nil
+}
+
+// Read multiple (5) rows.
+func (wf *OLTP) multiRead(r *rand.Rand) error {
+	return nil
+}
+
+// Blindly write a single row.
+func (wf *OLTP) blindWrite(r *rand.Rand) error {
+	for i := 0; i < NumSamples; i++ {
 		// For these tests, referential integrity is un-important since there are no defined
 		// foreign key constraints.
-		mutation := spanner.InsertMap(tableName, map[string]interface{}{
+		mutation := spanner.InsertMap(TransactionTableName, map[string]interface{}{
 			"id":         r.Int63(),
 			"companyId":  r.Int63(),
 			"fromUserId": r.Int63(),
@@ -57,23 +125,21 @@ func (wf *OLTP) blindWrite(r *rand.Rand) error {
 			return err
 		}
 	}
-
 	return nil
 }
 
+// Read and update a single row.
 func (wf *OLTP) readAndUpdate(r *rand.Rand) error {
-	const tableName = "Transactions"
-	const numSamples = 1000
 	const numTransactions = 20000000
 	const baseTransactionID int64 = 1000000000000000000
 
-	for i := 0; i < numSamples; i++ {
+	for i := 0; i < NumSamples; i++ {
 		// This should be both valid and random, hence we need to know the range of valid
 		// identifiers within the table.
 		updateID := baseTransactionID + (r.Int63() % numTransactions)
 		start := time.Now()
 		_, err := wf.client.ReadWriteTransaction(wf.ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
-			row, err := txn.ReadRow(wf.ctx, tableName, spanner.Key{updateID}, []string{"fromUserId", "toUserId"})
+			row, err := txn.ReadRow(wf.ctx, TransactionTableName, spanner.Key{updateID}, []string{"fromUserId", "toUserId"})
 			if err != nil {
 				return err
 			}
@@ -83,7 +149,7 @@ func (wf *OLTP) readAndUpdate(r *rand.Rand) error {
 			}
 
 			// Swapping the user IDs guarantees that referential integrity is maintained.
-			mutation := spanner.UpdateMap(tableName, map[string]interface{}{
+			mutation := spanner.UpdateMap(TransactionTableName, map[string]interface{}{
 				"id":         updateID,
 				"fromUserId": toUserID,
 				"toUserId":   fromUserID,
@@ -96,6 +162,5 @@ func (wf *OLTP) readAndUpdate(r *rand.Rand) error {
 			return err
 		}
 	}
-
 	return nil
 }
